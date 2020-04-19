72b7e9e5882b83170fafbf5b51e14854c4394f06
Glenn <57659894+dev-glennn@users.noreply.github.com>
2020-04-19 20:45:51 +0900

    feat: 회원가입 & nodemon 설치

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const {User} = require('./models/User')
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+
+// application/json
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://glenn:alswn4122!@boilerplate-cpkyn.mongodb.net/test?retryWrites=true&w=majority', {
@@ -11,6 +19,20 @@ mongoose.connect('mongodb+srv://glenn:alswn4122!@boilerplate-cpkyn.mongodb.net/t
 }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World! 안녕하세요'))
+
+app.post('/register', (req, res) => {
+
+    // 회언 가입 할 때 필요한 정보들을 client에서 가져오면
+    // 그것들을 database에 넣어준다.
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({success: false, err})
+        return res.status(200).json({success: true})
+    })
+
+
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
